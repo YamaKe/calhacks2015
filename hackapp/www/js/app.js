2cc2a234b83d204dc942fb0df2b8a2bec6193869
Kevin Lee <kevin651102@berkeley.edu>
2015-10-10 08:55:38 -0700

    Ionic Dev
    Login with user checks.

---
+++ hackapp/www/js/app.js
@@ -0,0 +1,43 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('mainp', ['ionic', 'mainp.controllers', 'mainp.services'])
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+	.state('login', {
+    	url: '/login',
+    	templateUrl: 'templates/login.html',
+    	controller: 'LoginCtrl'
+  	})
+
+  	.state('main', {
+    	url: '/main',
+        templateUrl: 'index.html'
+
+  	})
+
+ 	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/login');
+});
+
+// .run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//     // for form inputs)
+//     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//       cordova.plugins.Keyboard.disableScroll(true);
+
+//     }
+//     if (window.StatusBar) {
+//       // org.apache.cordova.statusbar required
+//       StatusBar.styleLightContent();
+//     }
+//   });
+// })
+
